5ca30906ef03fdfa02a0c78925c82ffeff519460
Marlaqk <ralph4scheu@gmail.com>
2020-12-05 08:43:25 +0100

    day_03

--- src/day_03/a.ts
+++ src/day_03/a.ts
@@ -7,11 +7,24 @@ const rl = readline.createInterface({
     terminal: false
 });
 
-let sum = 0;
+const moves = [
+    {right: 3, down: 1, trees: 0},
+]
+let lines: string[] = [];
 rl.on('line', (line) => {
-    sum += Number(line)
+    lines.push(line);
 })
 
 rl.on('close', () => {
-    console.log(sum)
+    console.log(moves.map((move) => {
+        let pos = 0;
+        for(let i = 0; i < lines.length; i+=move.down) {
+            if (lines[i][pos%31] == '#') {
+                move.trees++;
+            }
+            pos += move.right;
+        }
+        return move.trees;
+    }))
 });
+
--- src/day_03/b.ts
+++ src/day_03/b.ts
@@ -7,11 +7,27 @@ const rl = readline.createInterface({
     terminal: false
 });
 
-let sum = 0;
+const moves = [
+    {right: 1, down: 1, trees: 0},
+    {right: 3, down: 1, trees: 0},
+    {right: 5, down: 1, trees: 0},
+    {right: 7, down: 1, trees: 0},
+    {right: 1, down: 2, trees: 0},
+]
+let lines: string[] = [];
 rl.on('line', (line) => {
-    sum += Number(line)
+    lines.push(line);
 })
 
 rl.on('close', () => {
-    console.log(sum)
+    console.log(moves.map((move) => {
+        let pos = 0;
+        for(let i = 0; i < lines.length; i+=move.down) {
+            if (lines[i][pos%31] == '#') {
+                move.trees++;
+            }
+            pos += move.right;
+        }
+        return move.trees;
+    }).reduce((a, b)=> a*b))
 });
